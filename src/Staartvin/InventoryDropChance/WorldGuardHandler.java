da60fed9b954f98af34e3a2b92ecddfb7a1b9353
Vincent <mijnleraar@msn.com>
2013-06-29 18:00:35 +0200

    Added a getDeletePercentages method and a delete percentage WG flag

--- src/Staartvin/InventoryDropChance/WorldGuardHandler.java
+++ src/Staartvin/InventoryDropChance/WorldGuardHandler.java
@@ -11,90 +11,126 @@ import com.sk89q.worldguard.protection.managers.RegionManager;
 public class WorldGuardHandler {
 
 	private InventoryDropChance plugin;
-	
+
 	WorldGuardPlugin wgPlugin;
 	WGCustomFlagsPlugin customWGFlags;
 	public static IntegerFlag RETAIN_PERCENTAGE;
+	public static IntegerFlag DELETE_PERCENTAGE;
 	public static IntegerFlag XPLOSS_PERCENTAGE;
-	
+
 	public WorldGuardHandler(InventoryDropChance instance) {
 		plugin = instance;
 	}
-	
+
 	protected void registerFlags() {
-		if (!plugin.wgClass.isWorldGuardReady()) return;
-		
+		if (!plugin.wgClass.isWorldGuardReady())
+			return;
+
 		XPLOSS_PERCENTAGE = new IntegerFlag("xploss-percentage");
 		RETAIN_PERCENTAGE = new IntegerFlag("retain-percentage");
-		
+		DELETE_PERCENTAGE = new IntegerFlag("lose-percentage");
+
 		customWGFlags.addCustomFlag(XPLOSS_PERCENTAGE);
 		customWGFlags.addCustomFlag(RETAIN_PERCENTAGE);
+		customWGFlags.addCustomFlag(DELETE_PERCENTAGE);
 	}
-	
+
 	protected int getRetainPercentage(Player player) {
-		
+
 		if (plugin.wgClass.isWorldGuardReady()) {
-			RegionManager regionManager = wgPlugin.getRegionManager(player.getWorld());
+			RegionManager regionManager = wgPlugin.getRegionManager(player
+					.getWorld());
 			if (regionManager != null) {
-				ApplicableRegionSet set = regionManager.getApplicableRegions(player.getLocation());
+				ApplicableRegionSet set = regionManager
+						.getApplicableRegions(player.getLocation());
 				if (set != null) {
 					if (set.getFlag(RETAIN_PERCENTAGE) != null) {
 						return set.getFlag(RETAIN_PERCENTAGE);
 					}
 				}
-			}	
+			}
 		}
-		
-		for (String groupName: plugin.groups) {
+
+		for (String groupName : plugin.groups) {
 			if (player.hasPermission("idc.group." + groupName)) {
-				return plugin.getConfig().getInt("Groups." + groupName + ".retain percentage");
+				return plugin.getConfig().getInt(
+						"Groups." + groupName + ".retain percentage");
 			}
 		}
 		return 50;
 	}
-	
-	protected int getExpPercentage(Player player) {
-		
+
+	protected int getDeletePercentage(Player player) {
+
 		if (plugin.wgClass.isWorldGuardReady()) {
-			RegionManager regionManager = wgPlugin.getRegionManager(player.getWorld());
+			RegionManager regionManager = wgPlugin.getRegionManager(player
+					.getWorld());
 			if (regionManager != null) {
-				ApplicableRegionSet set = regionManager.getApplicableRegions(player.getLocation());
+				ApplicableRegionSet set = regionManager
+						.getApplicableRegions(player.getLocation());
+				if (set != null) {
+					if (set.getFlag(DELETE_PERCENTAGE) != null) {
+						return set.getFlag(DELETE_PERCENTAGE);
+					}
+				}
+			}
+		}
+
+		for (String groupName : plugin.groups) {
+			if (player.hasPermission("idc.group." + groupName)) {
+				return plugin.getConfig().getInt(
+						"Groups." + groupName + ".lose percentage");
+			}
+		}
+		return 50;
+	}
+
+	protected int getExpPercentage(Player player) {
+
+		if (plugin.wgClass.isWorldGuardReady()) {
+			RegionManager regionManager = wgPlugin.getRegionManager(player
+					.getWorld());
+			if (regionManager != null) {
+				ApplicableRegionSet set = regionManager
+						.getApplicableRegions(player.getLocation());
 				if (set != null) {
 					if (set.getFlag(XPLOSS_PERCENTAGE) != null) {
 						return set.getFlag(XPLOSS_PERCENTAGE);
 					}
 				}
-			}	
+			}
 		}
-		
-		for (String groupName: plugin.groups) {
+
+		for (String groupName : plugin.groups) {
 			if (player.hasPermission("idc.group." + groupName)) {
-				return plugin.getConfig().getInt("Groups." + groupName + ".xp loss");
+				return plugin.getConfig().getInt(
+						"Groups." + groupName + ".xp loss");
 			}
 		}
 		return 50;
 	}
-	
+
 	protected WorldGuardPlugin getWorldGuard() {
-	    wgPlugin = (WorldGuardPlugin) plugin.getServer().getPluginManager().getPlugin("WorldGuard");
-	 
-	    // WorldGuard may not be loaded
-	    if (wgPlugin == null || !(wgPlugin instanceof WorldGuardPlugin)) {
-	        return null; // Maybe you want throw an exception instead
-	    }
-	 
-	    return (WorldGuardPlugin) wgPlugin;
+		wgPlugin = (WorldGuardPlugin) plugin.getServer().getPluginManager()
+				.getPlugin("WorldGuard");
+
+		// WorldGuard may not be loaded
+		if (wgPlugin == null || !(wgPlugin instanceof WorldGuardPlugin)) {
+			return null; // Maybe you want throw an exception instead
+		}
+
+		return (WorldGuardPlugin) wgPlugin;
 	}
 
-	protected WGCustomFlagsPlugin getWGCustomFlags()
-	{
-		customWGFlags = (WGCustomFlagsPlugin) plugin.getServer().getPluginManager().getPlugin("WGCustomFlags");
-	  
-	  if (customWGFlags == null || !(customWGFlags instanceof WGCustomFlagsPlugin))
-	  {
-	    return null;
-	  }
+	protected WGCustomFlagsPlugin getWGCustomFlags() {
+		customWGFlags = (WGCustomFlagsPlugin) plugin.getServer()
+				.getPluginManager().getPlugin("WGCustomFlags");
 
-	  return (WGCustomFlagsPlugin) customWGFlags;
+		if (customWGFlags == null
+				|| !(customWGFlags instanceof WGCustomFlagsPlugin)) {
+			return null;
+		}
+
+		return (WGCustomFlagsPlugin) customWGFlags;
 	}
 }
